a218aeb275761bfc3ccb0a80f7a010fd84565654
brainagenet <ms29.seo@gmail.com>
2011-06-24 14:23:20 +0900

    추가된 API에 대한 단위 테스트 추가.

--- src/test/java/net/brainage/rfc/util/svn/SvnClientTest.java
+++ src/test/java/net/brainage/rfc/util/svn/SvnClientTest.java
@@ -6,6 +6,13 @@ package net.brainage.rfc.util.svn;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.brainage.rfc.util.svn.handler.AbstractSvnDiffStatusHandler;
+import net.brainage.rfc.util.svn.handler.DefaultSvnDiffStatusHandler;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -41,6 +48,20 @@ public class SvnClientTest
     }
 
     @Test
+    public void testAdd() {
+        String filePath = "C:/Users/exia/rfcmanager/workspaces/wc/pcw-cmm/pcw-test-ear/bbb/b.txt";
+        List<File> commitEntries = new ArrayList<File>();
+        try {
+            svnClient.add(new File(filePath), commitEntries);
+            System.out.println("SvnClientTest.testAdd() :: commit entries size = "
+                    + commitEntries.size());
+            assertTrue(commitEntries.size() == 2);
+        } catch ( SVNException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void testGetFile() {
         String repoUrlPath = "svn://121.253.45.167/pcw-smg/trunk/pcw-smg-ear";
         String urlPath = "pcw-smg-svc/src/java/com/sec/pcw/smg/service/PushServiceResourceImpl.java";
@@ -58,17 +79,33 @@ public class SvnClientTest
      */
     @Test
     public void testDiffStatus() {
-        String urlPath1 = "svn://121.253.45.167/pcw-smg/trunk/pcw-smg-ear/pcw-smg-svc/src/java/com/sec/pcw/smg/service/PushServiceResourceImpl.java";
-        long revision1 = 522;
+        // svn://121.253.45.167/pcw-cmm/trunk/pcw-test-ear/bbb/b.txt
+        // String urlPath1 = "svn://121.253.45.167/pcw-smg/trunk/pcw-smg-ear/pcw-smg-svc/src/java/com/sec/pcw/smg/service/PushServiceResourceImpl.java";
+        String urlPath1 = "svn://121.253.45.167/pcw-cmm/trunk/pcw-test-ear/bbb/b.txt";
+        // long revision1 = 522;
+        long revision1 = 182;
 
-        String urlPath2 = "svn://121.253.45.167/pcw-smg/branches/pcw-smg-ear/pcw-smg-svc/src/java/com/sec/pcw/smg/service/PushServiceResourceImpl.java";
+        // String urlPath2 = "svn://121.253.45.167/pcw-smg/branches/pcw-smg-ear/pcw-smg-svc/src/java/com/sec/pcw/smg/service/PushServiceResourceImpl.java";
+        String urlPath2 = "svn://121.253.45.167/pcw-cmm/branches/pcw-test-ear/bbb/b.txt";
 
         try {
-            char actual = svnClient.diffStatus(urlPath1, revision1, urlPath2, null);
+            AbstractSvnDiffStatusHandler handler = new DefaultSvnDiffStatusHandler();
+            char actual = svnClient.diffStatus(urlPath1, revision1, urlPath2, handler);
+            System.out.println(actual);
             assertEquals('M', actual);
         } catch ( SVNException e ) {
+            e.printStackTrace();
         }
+    }
 
+    @Test
+    public void testDelete() {
+        try {
+            String t = "C:/Users/exia/rfcmanager/workspaces/wc/pcw-cmm/pcw-test-ear/a.txt";
+            svnClient.delete(new File(t));
+        } catch ( SVNException e ) {
+            e.printStackTrace();
+        }
     }
 
 }
